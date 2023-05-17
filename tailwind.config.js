c20b0b7d4edcdeb47e417a46bf00517d59da0eaa
Vinicius Matias <vinicius.lima@virtus.ufcg.edu.br>
2023-05-17 19:00:36 -0300

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container:{
+     // center: true,
+      padding: '2rem'
+    },
+    extend: {},
+  },
+  plugins: [
+  
+  ],
+}
