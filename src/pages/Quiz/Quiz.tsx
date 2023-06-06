3d2455ff9f0db5c5e0ec72de4f9cc30f10fd55af
Douglas Sebastian <kagnanii.miyo@gmail.com>
2023-06-06 13:33:08 -0300

    feat: send response

--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -1,13 +1,15 @@
-import { ReactNode } from "react";
+import { FormEvent, ReactNode, useState } from "react";
 import * as RadioGroup from '@radix-ui/react-radio-group';
 import { Question } from "../../types/question";
 
 import './Quiz.css'
 import { Alternative } from "../../types/alternative";
 import { useParams } from "react-router-dom";
+import { Feedback } from "../../types/feedback";
 
 
 export function QuizPage() {
+    const [responseSent, setResponseSent] = useState<boolean>(false);
     const { id } = useParams();
     // todo: check if id is undefined
 
@@ -28,35 +30,67 @@ export function QuizPage() {
         { correct: false, id: 5, question_id: 1, title: "LibreOffice, Virtualbox e Eclipse" }
     ]
 
+    // todo: get feedback from database
+    const feedback: Feedback = {
+        id: 1,
+        feedback_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+        link: "https://pt.wikipedia.org/wiki/Lorem_ipsum"
+    }
+
+    const disabled = responseSent
+        ? { 'data-disabled': '' }
+        : { };
+
     return (
-        <div id="question">
-            {/* todo: subject comes from API */}
-            <div className="subject">
-                <h2>ASSUNTO</h2>
+        <div>
+            <div id="question">
+                {/* todo: subject comes from API */}
+                <div className="subject">
+                    <h2>ASSUNTO</h2>
+                </div>
+                <div className="progress-container">
+                    {/* todo: calculate progress */}
+                    <div className="progress-current" />
+                </div>
+                <form className="question-container" onSubmit={sendResponse} >
+                    <h2>{question.title}</h2>
+                    <RadioGroup.Root name="response" required={true} disabled={responseSent} >
+                    {
+                        alternatives.map((alternative, index) => {
+                            const alternativeId = `alternative-${index}`;
+                            return (
+                                <div className="alternative-container" key={alternative.id}>
+                                    <RadioGroup.Item className="alternative" value={`${alternative.id}`} id={alternativeId}>
+                                        <RadioGroup.Indicator className="alternative-indicator" />
+                                    </RadioGroup.Item>
+                                    <label htmlFor={alternativeId}>{alternative.title}</label>
+                                </div>
+                            );
+                        })
+                    }
+                    </RadioGroup.Root>
+                    <button className="submit" type="submit">{responseSent ? "Próxima questão" : "Enviar resposta"}</button>
+                </form>
             </div>
-            <div className="progress-container">
-                {/* todo: calculate progress */}
-                <div className="progress-current" />
-            </div>
-            <form className="question-container">
-                <h2>{question.title}</h2>
-                <RadioGroup.Root name="response">
-                 {
-                    alternatives.map((alternative, index) => {
-                        const alternativeId = `alternative-${index}`;
-                        return (
-                            <div className="alternative-container" key={alternative.id}>
-                                <RadioGroup.Item className="alternative" value={`${alternative.id}`} id={alternativeId}>
-                                    <RadioGroup.Indicator className="alternative-indicator" />
-                                </RadioGroup.Item>
-                                <label htmlFor={alternativeId}>{alternative.title}</label>
+
+            {
+                responseSent
+                    ? <div id="feedback-container">
+                            <div style={{width: 'fit-content'}}>
+                                <div className="feedback-bar" />
+                                <h3>Segue a dica:</h3>
                             </div>
-                        );
-                    })
-                 }
-                </RadioGroup.Root>
-                 <button className="submit" type="submit">Enviar Resposta</button>
-            </form>
+                            <p className="feedback">{feedback.feedback_text}</p>
+                            <a href={feedback.link} className="see-more" target="_blank">Entenda mais</a>
+                        </div>
+
+                    : <></>
+            }
         </div>
     );
+
+    function sendResponse(event: FormEvent) {
+        event.preventDefault();
+        setResponseSent(true);
+    }
 }
