2387260fce0e58a6dc533a6280ac8f09f2a75ecb
Douglas Sebastian <62383247+NiiMiyo@users.noreply.github.com>
2023-06-22 09:52:10 -0300

    change: configurar jest (#19)
    * change: install @babel/preset-typescript
    
    * change: moduleNameMapper matches tsconfig paths
    
    * change: jest coverageDirectory inside .jest/

--- __tests__/sample.test.tsx
+++ __tests__/sample.test.tsx
@@ -1,3 +1,18 @@
-test("1 to equal 1", () => {
-  expect(1).toBe(1);
-});
+import type { Question } from "@/types/question";
+
+describe("sample tests used as examples", () => {
+    test("1 to equal 1", () => {
+      expect(1).toBe(1);
+    });
+
+    test("import type from src", () => {
+      const q: Question = {
+        feedback_id: 0,
+        id: 1,
+        title: "example",
+        user_create_id: 0
+      };
+
+      expect(q.id).toBe(1);
+    })
+})
