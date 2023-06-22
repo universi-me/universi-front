007e9d73ae9f6bbdc5edcece0389ecf371e5dcfa
Vinicius Matias <vinicius.lima@virtus.ufcg.edu.br>
2023-06-22 08:52:57 -0300

    feat: Inserindo o authProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import { router } from "./services/routes";
 import {RouterProvider} from 'react-router-dom'
 import './global.css'
+import { AuthProvider } from "./contexts/Auth/AuthProvider";
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <AuthProvider>
+      <RouterProvider router={router}/>
+    </AuthProvider>
+
   </React.StrictMode>
 );
