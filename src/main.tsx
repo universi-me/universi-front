541965cb807497ec7c5bf6b116792988d9a93643
Douglas Sebastian <kagnanii.miyo@gmail.com>
2023-07-14 16:41:00 -0300

    change: convert global.css to .less

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { router } from "@/services/routes";
 import {RouterProvider} from 'react-router-dom'
-import './global.css'
+import '@/layouts/global.less'
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
