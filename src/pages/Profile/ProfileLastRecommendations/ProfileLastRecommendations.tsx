7e164078341821d0d9243a7a8657d1c6a3735251
Douglas Sebastian <kagnanii.miyo@gmail.com>
2023-08-04 09:58:51 -0300

    change: using recommendations from api

--- src/pages/Profile/ProfileLastRecommendations/ProfileLastRecommendations.tsx
+++ src/pages/Profile/ProfileLastRecommendations/ProfileLastRecommendations.tsx
@@ -1,40 +1,50 @@
+import { useContext } from 'react';
 import './ProfileLastRecommendations.css'
+import { ProfileContext } from '../ProfileContext';
+import { getFullName } from '@/utils/profileUtils';
 
-export type ProfileLastRecommendationsProps = {
-    recommendations: string[];
-};
+const MAX_RECOMMENDATIONS_QUANTITY = 3;
 
-export function ProfileLastRecommendations(props: ProfileLastRecommendationsProps) {
+export function ProfileLastRecommendations() {
+    const profileContext = useContext(ProfileContext);
+    if (profileContext === null)
+        return null;
+
     return (
         <div className="last-recommendations">
             <h2 className="heading">Últimas Recomendações</h2>
-            <div className="list">
-                {
-                    props.recommendations.map(recommendation => {
-                        return (
-                            <div className="recommendation">
-                                {/* todo: profile image from API */}
-                                <div className="image" style={{backgroundColor: "#8A8A8A"}} />
+            { profileContext.profile.recomendacoesRecebidas.length > 0 ?
+                <div className="list">
+                    {
+                        profileContext.profile.recomendacoesRecebidas.map((recommendation, i) => {
+                            if (i >= MAX_RECOMMENDATIONS_QUANTITY)
+                                return null;
 
-                                <div className="box">
-                                    {/* todo: user name from API */}
-                                    <h2 className="user-name">{"Usuário Aleatório"}</h2>
-                                    <h3 className="recommended-by">Recomendou pela competência:</h3>
-                                    {/* todo: competence name */}
-                                    <h3 className="competence-name">{"Nome da competência"}</h3>
-                                </div>
+                            const hasOriginImage = recommendation.origin.image === null;
+                            return (
+                                <div className="recommendation" key={recommendation.id}>
+                                    <div className="image" style={ hasOriginImage
+                                        ? {backgroundImage: recommendation.origin.image as string}
+                                        : {backgroundColor: "#8A8A8A"}
+                                    } />
 
-                                <div className="quote-box">
-                                    {/* todo: quote from API */}
-                                    <p className="quote">{"Lorem ipsum dolor sit amet. Hic dolor reiciendis rem earum voluptatem sit similique magnam est repellat mollitia."}</p>
-                                    <img src="/assets/icons/quotes-1.svg" className="icon" />
+                                    <div className="box">
+                                        <h2 className="user-name">{getFullName(recommendation.origin)}</h2>
+                                        <h3 className="recommended-by">Recomendou pela competência:</h3>
+                                        <h3 className="competence-name">{recommendation.competenceType.name}</h3>
+                                    </div>
+
+                                    <div className="quote-box">
+                                        <p className="quote">{recommendation.description}</p>
+                                        <img src="/assets/icons/quotes-1.svg" className="icon" />
+                                    </div>
                                 </div>
-                            </div>
-                        );
-                    })
-                }
-            </div>
-            <button className="show-more">Ver mais</button>
+                            );
+                        })
+                    }
+                    <button className="show-more">Ver mais</button>
+                </div>
+            : <p className="empty-recommendations">Nenhuma recomendação recebida.</p> }
         </div>
     );
 }
--- src/types/Profile.ts
+++ src/types/Profile.ts
@@ -1,24 +1,25 @@
 import type { User } from "@/types/User";
 import type { Competence } from "@/types/Competence";
+import type { Recommendation } from "@/types/Recommendation";
 
 export type Gender = "M" | "F" | "O";
 
 export type Profile = {
-    id:           number;
-    user:         User;
-    usuario:      User;
-    firstname:    string | null;
-    gender:       Gender | null;
-    image:        string | null;
-    lastname:     string | null;
-    sexo:         Gender | null;
-    bio:          string | null;
-    creationDate: string;
-    competences:  Competence[];
-    groups:       number[];
+    id:                     number;
+    user:                   User;
+    usuario:                User;
+    firstname:              string | null;
+    gender:                 Gender | null;
+    image:                  string | null;
+    lastname:               string | null;
+    sexo:                   Gender | null;
+    bio:                    string | null;
+    creationDate:           string;
+    competences:            Competence[];
+    groups:                 number[];
+    recomendacoesFeitas:    Recommendation[];
+    recomendacoesRecebidas: Recommendation[];
 
     // todo: type this properties
     links:                  any[];
-    recomendacoesFeitas:    any[];
-    recomendacoesRecebidas: any[];
 }
