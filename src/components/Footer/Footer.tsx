dfb5447a2a1f78c5ebc3675df54dae86f1127dfe
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-08-19 11:09:12 -0300

    change: update footer to color palette

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,17 +11,17 @@ const Footer: React.FC = () => {
       <div className="line"></div>
       <div className="buttons-container">
         <a href="" className="circular-button">
-          <img src="/assets/icons/github-white.svg" alt="Icon Rede Social 1" />
+          <img src="/assets/icons/github-black.svg" alt="Icon Rede Social 1" />
         </a>
         <a href="" className="circular-button">
-          <img src="/assets/icons/instagram-white.svg" alt="Icon Rede Social 2" />
+          <img src="/assets/icons/instagram-black.svg" alt="Icon Rede Social 2" />
         </a>
         <a href="" className="circular-button">
-          <img src="/assets/icons/facebook-white.svg" alt="Icon Rede Social 3" />
+          <img src="/assets/icons/facebook-black.svg" alt="Icon Rede Social 3" />
         </a>
       </div>
       <button className="scroll-to-top-button" onClick={handleScrollToTop}>
-        <img src="/assets/icons/chevron-up-1.svg" alt="Scroll to Top" />
+        <img src="/assets/icons/chevron-up-black.svg" alt="Scroll to Top" />
       </button>
       <h3 className="copyright">© Copyright Universi.me. All Rights Reserved</h3>
     </div>
