38b0b7b2af3c8a9ca415ac5fbbbb2b97d478971f
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-08-30 21:30:33 -0300

    change: added typing for group creation

--- src/services/UniversimeApi/Group.ts
+++ src/services/UniversimeApi/Group.ts
@@ -1,10 +1,18 @@
-import { GroupType } from "@/types/Group";
-import type { Group } from "@/types/Group";
+import type { Group, GroupType } from "@/types/Group";
 import type { Profile } from "@/types/Profile";
 import type { ApiResponse } from "@/types/UniversimeApi";
 import axios from "axios";
 
-export type GroupCreateDTO = {
+const groupApi = axios.create({
+    baseURL: `${import.meta.env.VITE_UNIVERSIME_API}/group`,
+    withCredentials: true,
+});
+
+export type GroupId_RequestDTO = {
+    groupId: string;
+};
+
+export type GroupCreate_RequestDTO = {
     name:            string;
     description:     string;
     nickname:        string;
@@ -14,31 +22,7 @@ export type GroupCreateDTO = {
     isPublic:        boolean;
     canJoin:         boolean;
     isRootGroup:     boolean;
-    parentGroupId?:  number;
-};
-
-const groupApi = axios.create({
-    baseURL: `${import.meta.env.VITE_UNIVERSIME_API}/group`,
-    withCredentials: true,
-});
-
-export async function create(body: GroupCreateDTO) {
-    return (await groupApi.post("/create", {
-        groupRoot:      body.isRootGroup,
-        groupId:        body.parentGroupId?.toString(),
-        nickname:       body.nickname,
-        name:           body.name,
-        description:    body.description,
-        imageUrl:       body.imageUrl,
-        type:           body.groupType,
-        canCreateGroup: body.canHaveSubgroup,
-        publicGroup:    body.isPublic,
-        canEnter:       body.canJoin,
-    })).data
-}
-
-export type GroupId_RequestDTO = {
-    groupId: string;
+    parentGroupId?:  string;
 };
 
 export type GroupIdOrPath_RequestDTO = {
@@ -47,6 +31,7 @@ export type GroupIdOrPath_RequestDTO = {
 };
 
 export type GroupGet_ResponseDTO =          ApiResponse<{ group: Group }>;
+export type GroupCreate_ResponseDTO =       ApiResponse;
 export type GroupSubgroups_ResponseDTO =    ApiResponse<{ subgroups: Group[] }>;
 export type GroupParticipants_ResponseDTO = ApiResponse<{ participants: Profile[] }>;
 export type GroupJoin_ResponseDTO =         ApiResponse;
@@ -56,6 +41,21 @@ export async function get(body: GroupIdOrPath_RequestDTO) {
     return (await groupApi.post<GroupGet_ResponseDTO>('/get', {
         groupId:   body.groupId,
         groupPath: body.groupPath,
+    })).data;
+}
+
+export async function create(body: GroupCreate_RequestDTO) {
+    return (await groupApi.post<GroupCreate_ResponseDTO>("/create", {
+        groupRoot:      body.isRootGroup,
+        groupId:        body.parentGroupId,
+        nickname:       body.nickname,
+        name:           body.name,
+        description:    body.description,
+        imageUrl:       body.imageUrl,
+        type:           body.groupType,
+        canCreateGroup: body.canHaveSubgroup,
+        publicGroup:    body.isPublic,
+        canEnter:       body.canJoin,
     })).data;
 }
 
