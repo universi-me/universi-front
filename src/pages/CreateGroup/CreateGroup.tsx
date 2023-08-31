1b1766fec8a5ebe7fc997dafc22757c112e92b8c
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-08-31 08:51:57 -0300

    change: base css for inputs and textarea

--- src/pages/CreateGroup/CreateGroup.tsx
+++ src/pages/CreateGroup/CreateGroup.tsx
@@ -1,69 +1,74 @@
 import UniversimeApi from "@/services/UniversimeApi";
 import { GroupType, GroupTypeToLabel } from "@/types/Group";
+import "./CreateGroup.less"
 
 export function CreateGroupPage() {
 
     // todo: Change defaultValues if updating existing group
-    return <form id="create-group-page">
-        <fieldset className="name">
-            <legend>Nome do grupo</legend>
-            <input type="text" name="name" />
-        </fieldset>
+    return <div id="create-group-page">
+        {/* Update heading to "Editar grupo" when editing */}
+        <div className="heading">Criar grupo</div>
+        <form className="manage-group-form">
+            <fieldset className="name">
+                <legend>Nome do grupo</legend>
+                <input type="text" name="name" placeholder="Insira o nome do grupo" />
+            </fieldset>
 
-        <fieldset className="nickname">
-            <legend>Nickname do grupo</legend>
-            <input type="text" name="nickname" />
-            {/* todo: disable nickname if updating existing group */}
-        </fieldset>
+            <fieldset className="nickname">
+                <legend>Nickname do grupo</legend>
+                <input type="text" name="nickname" placeholder="Insira o nickname do grupo" />
+                {/* todo: disable nickname if updating existing group */}
+            </fieldset>
 
-        <fieldset className="description">
-            <legend>Descrição do grupo</legend>
-            <textarea name="description" />
-        </fieldset>
+            <fieldset className="description">
+                <legend>Descrição do grupo</legend>
+                <textarea name="description" placeholder="Insira a descrição do grupo" />
+            </fieldset>
 
-        <fieldset className="type">
-            <legend>Tipo do grupo</legend>
-            <select defaultValue={""} name="type">
-                <option disabled value="">Selecione o tipo do grupo</option>
-                {
-                    Object.entries(GroupTypeToLabel)
-                        .map(([groupType, groupTypeLabel]) => {
-                            return <option value={groupType} key={groupType}>{groupTypeLabel}</option>
-                        })
-                }
-            </select>
-        </fieldset>
+            <fieldset className="type">
+                <legend>Tipo do grupo</legend>
+                <select defaultValue={""} name="type">
+                    <option disabled value="">Selecione o tipo do grupo</option>
+                    {
+                        Object.entries(GroupTypeToLabel)
+                            .map(([groupType, groupTypeLabel]) => {
+                                return <option value={groupType} key={groupType}>{groupTypeLabel}</option>
+                            })
+                    }
+                </select>
+            </fieldset>
 
-        <fieldset className="image">
-            <legend>Imagem do grupo</legend>
-            <img src="" alt="" />
-            <input type="url" name="image" />
-        </fieldset>
+            <fieldset className="image">
+                <legend>Imagem do grupo</legend>
+                <img src="" alt="" />
+                <input type="url" name="image" placeholder="Insira a URL para a imagem do grupo" />
+            </fieldset>
 
-        <fieldset className="canCreateGroup">
-            <legend>Pode ter subgrupos?</legend>
-            <input type="checkbox" name="canCreateGroup" />
-        </fieldset>
+            <fieldset className="canCreateGroup">
+                <legend>Pode ter subgrupos?</legend>
+                <input type="checkbox" name="canCreateGroup" />
+            </fieldset>
 
-        <fieldset className="publicGroup">
-            <legend>É um grupo público?</legend>
-            <input type="checkbox" name="publicGroup" />
-        </fieldset>
+            <fieldset className="publicGroup">
+                <legend>É um grupo público?</legend>
+                <input type="checkbox" name="publicGroup" />
+            </fieldset>
 
-        <fieldset className="canEnter">
-            <legend>Qualquer pessoa pode entrar?</legend>
-            <input type="checkbox" name="canEnter" />
-        </fieldset>
+            <fieldset className="canEnter">
+                <legend>Qualquer pessoa pode entrar?</legend>
+                <input type="checkbox" name="canEnter" />
+            </fieldset>
 
-        <fieldset className="groupId">
-            <legend>Grupo pai</legend>
-            <input type="checkbox" name="groupRoot" />
-            {/* todo: select parent group */}
-        </fieldset>
+            <fieldset className="groupId">
+                <legend>Grupo pai</legend>
+                <input type="checkbox" name="groupRoot" />
+                {/* todo: select parent group */}
+            </fieldset>
 
-        <button type="button" onClick={createGroup}>Criar grupo</button>
-        {/* todo: change to "Atualizar grupo" if updating existing group */}
-    </form>;
+            <button type="button" onClick={createGroup}>Criar grupo</button>
+            {/* todo: change to "Atualizar grupo" if updating existing group */}
+        </form>
+    </div>;
 }
 
 function getValuesFromPage() {
