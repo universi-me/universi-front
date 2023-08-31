915bb23b492035eab8056d88261eaf67924e3245
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-08-31 12:10:28 -0300

    change: edit group
    * added functionality to edit an existing group
    * added default values when editing a group
    * disabled unchangeable values when editing a group

--- src/pages/CreateGroup/CreateGroup.tsx
+++ src/pages/CreateGroup/CreateGroup.tsx
@@ -1,59 +1,34 @@
+import Select from "react-select";
+import * as Switch from "@radix-ui/react-switch"
+import { useLoaderData } from "react-router-dom";
+
 import UniversimeApi from "@/services/UniversimeApi";
 import { GroupType, GroupTypeToLabel } from "@/types/Group";
-import { useEffect, useState } from "react";
-import Select, { OptionsOrGroups } from "react-select";
-import * as Switch from "@radix-ui/react-switch"
+import { EDIT_GROUP_PARAMETER, ManageGroupLoaderResponse } from "@/pages/CreateGroup";
+
 import "./CreateGroup.less"
 
-type ReactSelectOption = {
-    label: string;
-    value: string;
-};
-
 export function CreateGroupPage() {
-    const [availableParents, setAvailableParents] = useState<OptionsOrGroups<ReactSelectOption, never>>([]);
-    const availableGroupTypes: ReactSelectOption[] = Object.entries(GroupTypeToLabel).map(([groupType, label]) => {
-        return {
-            label,
-            value: groupType,
-        };
-    });
+    const { availableGroupTypes, availableParents, editedGroup } = useLoaderData() as ManageGroupLoaderResponse;
 
-    useEffect(() => {
-        UniversimeApi.Group.availableParents()
-            .then(res => {
-                if (res.success && res.body !== undefined) {
-                    const options = res.body.groups.map((g): ReactSelectOption => {
-                        return {
-                            label: g.name,
-                            value: g.id,
-                        };
-                    });
-
-                    setAvailableParents(options);
-                }
-            })
-    }, []);
-
-    // todo: Change defaultValues if updating existing group
     return <div id="create-group-page">
-        {/* Update heading to "Editar grupo" when editing */}
-        <div className="heading">Criar grupo</div>
+        <div className="heading">{ !editedGroup ? "Criar grupo" : "Editar grupo" }</div>
         <form className="manage-group-form">
             <fieldset className="name">
                 <legend>Nome do grupo</legend>
-                <input type="text" name="name" placeholder="Insira o nome do grupo" />
+                <input type="text" name="name" placeholder="Insira o nome do grupo" defaultValue={editedGroup?.name ?? ""} />
             </fieldset>
 
             <fieldset className="nickname">
                 <legend>Nickname do grupo</legend>
-                <input type="text" name="nickname" placeholder="Insira o nickname do grupo" />
-                {/* todo: disable nickname if updating existing group */}
+                <input type="text" name="nickname" placeholder="Insira o nickname do grupo"
+                    disabled={!!editedGroup} defaultValue={editedGroup?.nickname ?? ""}
+                    title={!editedGroup ? undefined : "Você não pode mudar o nickname de um grupo que já existe"} />
             </fieldset>
 
             <fieldset className="description">
                 <legend>Descrição do grupo</legend>
-                <textarea name="description" placeholder="Insira a descrição do grupo" />
+                <textarea name="description" placeholder="Insira a descrição do grupo" defaultValue={editedGroup?.description ?? ""} />
             </fieldset>
 
             <fieldset className="type">
@@ -65,32 +40,37 @@ export function CreateGroupPage() {
                     className="react-select"
                     classNamePrefix="react-select-option"
                     isClearable={false}
+
+                    defaultValue={
+                        editedGroup?.type === undefined
+                            ? undefined
+                            : { label: GroupTypeToLabel[editedGroup.type], value: editedGroup.type.toString()}
+                    }
                 />
             </fieldset>
 
             <fieldset className="image">
                 <legend>Imagem do grupo</legend>
-                <img src="" alt="" />
-                <input type="url" name="image" placeholder="Insira a URL para a imagem do grupo" />
+                <input type="url" name="image" placeholder="Insira a URL para a imagem do grupo" defaultValue={editedGroup?.image ?? ""} />
             </fieldset>
 
             <fieldset className="canCreateGroup one-line-fieldset">
                 <div className="fieldset-legend">Pode ter subgrupos</div>
-                <Switch.Root name="canCreateGroup" className="radix-switch-root">
+                <Switch.Root name="canCreateGroup" className="radix-switch-root" defaultChecked={editedGroup?.canCreateGroup ?? false} >
                     <Switch.Thumb className="radix-switch-thumb" />
                 </Switch.Root>
             </fieldset>
 
             <fieldset className="publicGroup one-line-fieldset">
                 <div className="fieldset-legend">É um grupo público?</div>
-                <Switch.Root name="publicGroup" className="radix-switch-root">
+                <Switch.Root name="publicGroup" className="radix-switch-root" defaultChecked={editedGroup?.publicGroup ?? false} >
                     <Switch.Thumb className="radix-switch-thumb" />
                 </Switch.Root>
             </fieldset>
 
             <fieldset className="canEnter one-line-fieldset">
                 <div className="fieldset-legend">Qualquer pessoa pode entrar?</div>
-                <Switch.Root name="canEnter" className="radix-switch-root">
+                <Switch.Root name="canEnter" className="radix-switch-root" defaultChecked={editedGroup?.canEnter ?? false} >
                     <Switch.Thumb className="radix-switch-thumb" />
                 </Switch.Root>
             </fieldset>
@@ -105,11 +85,19 @@ export function CreateGroupPage() {
                     classNamePrefix="react-select-option"
                     isClearable={true}
                     isSearchable={true}
+
+                    isDisabled={ !!editedGroup }
+                    defaultValue={
+                        !editedGroup
+                            ? undefined 
+                            : { label: "Você não pode mudar o grupo pai de um grupo já existente", value: "" }
+                    }
                 />
             </fieldset>
 
-            <button className="save-group" type="button" onClick={createGroup}>Criar grupo</button>
-            {/* todo: change to "Atualizar grupo" if updating existing group */}
+            <button className="save-group" type="button" onClick={!editedGroup ? createGroup : editGroup}>
+                { !editedGroup ? "Criar grupo" : "Atualizar grupo" }
+            </button>
         </form>
     </div>;
 }
@@ -118,62 +106,76 @@ function getValuesFromPage() {
     const pageElement = document.querySelector("#create-group-page") as HTMLFormElement;
 
     const nameElement = pageElement.querySelector('input[name="name"]') as HTMLInputElement;
-    const nicknameElement = pageElement.querySelector('input[name="nickname"]') as HTMLInputElement;
     const descriptionElement = pageElement.querySelector('textarea[name="description"]') as HTMLTextAreaElement;
-    const groupTypeElement = pageElement.querySelector('select[name="type"]') as HTMLSelectElement;
+    const groupTypeElement = pageElement.querySelector('input[name="type"]') as HTMLSelectElement;
     const imageElement = pageElement.querySelector('input[name="image"]') as HTMLInputElement;
     const canCreateGroupElement = pageElement.querySelector('input[name="canCreateGroup"]') as HTMLInputElement;
     const publicGroupElement = pageElement.querySelector('input[name="publicGroup"]') as HTMLInputElement;
     const canEnterElement = pageElement.querySelector('input[name="canEnter"]') as HTMLInputElement;
-    const parentGroupId = pageElement.querySelector('input[name="parentGroupId"]') as HTMLInputElement;
-
-    const hasParentGroup = parentGroupId.value !== "";
 
     return {
         name: nameElement.value,
         description: descriptionElement.value,
-        nickname: nicknameElement.value,
         type: groupTypeElement.value as GroupType,
         imageUrl: imageElement.value,
         canCreateGroup: canCreateGroupElement.checked,
         publicGroup: publicGroupElement.checked,
         canEnter: canEnterElement.checked,
-        groupRoot: !hasParentGroup,
-        parentGroupId: hasParentGroup ? parentGroupId.value : undefined,
     };
 }
 
-function createGroup() {
+function getCreateOnlyValuesFromPage() {
+    const pageElement = document.querySelector("#create-group-page") as HTMLFormElement;
+
+    const nicknameElement = pageElement.querySelector('input[name="nickname"]') as HTMLInputElement;
+    const parentGroupId = pageElement.querySelector('input[name="parentGroupId"]') as HTMLInputElement;
+    const hasParentGroup = parentGroupId.value !== "";
+
+    return {
+        nickname: nicknameElement.value,
+        groupRoot: !hasParentGroup,
+        parentGroupId: hasParentGroup ? parentGroupId.value : undefined,
+    }
+}
+
+function editGroup() {
+    const groupId = new URL(document.URL).searchParams.get(EDIT_GROUP_PARAMETER);
+    if (groupId === null) {
+        createGroup();
+        return;
+    }
+
     const values = getValuesFromPage();
-    UniversimeApi.Group.create({
+    UniversimeApi.Group.update({
+        groupId,
+
         name:            values.name,
-        nickname:        values.nickname,
         description:     values.description,
         groupType:       values.type,
         canHaveSubgroup: values.canCreateGroup,
         isPublic:        values.publicGroup,
         canJoin:         values.canEnter,
         imageUrl:        values.imageUrl,
-        isRootGroup:     values.groupRoot,
-        parentGroupId:   values.parentGroupId,
+    });
+}
+
+function createGroup() {
+    const values = getValuesFromPage();
+    const createValues = getCreateOnlyValuesFromPage();
+
+    UniversimeApi.Group.create({
+        name:            values.name,
+        nickname:        createValues.nickname,
+        description:     values.description,
+        groupType:       values.type,
+        canHaveSubgroup: values.canCreateGroup,
+        isPublic:        values.publicGroup,
+        canJoin:         values.canEnter,
+        imageUrl:        values.imageUrl,
+        isRootGroup:     createValues.groupRoot,
+        parentGroupId:   createValues.parentGroupId,
     })
         .then(r => {
             console.dir(r)
         });
 }
-
-/* 
-Inputs:
-    - name : string
-    - description : string
-    - type : GroupType
-    - imageUrl : string (URL)
-    - canCreateGroup : boolean (Group can only have subgroup if true)
-    - publicGroup : boolean
-    - canEnter : boolean
-
-Create-only inputs:
-    - groupRoot : boolean (Group is Master Group if true)
-    - groupId : string (Parent group id)
-    - nickname : string
-*/
