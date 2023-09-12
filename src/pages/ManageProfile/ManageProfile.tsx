ec8954d7498a352f282a7a2a665788f00dbe02a5
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-09-12 08:19:21 -0300

    feat: base for manage profile page

---
+++ src/pages/ManageProfile/ManageProfile.tsx
@@ -0,0 +1,7 @@
+import "./ManageProfile.less";
+
+export function ManageProfilePage() {
+    return <div id="manage-profile-page">
+
+    </div>;
+}
---
+++ src/pages/ManageProfile/index.ts
@@ -0,0 +1,4 @@
+import { ManageProfilePage } from "./ManageProfile";
+export default ManageProfilePage;
+
+export * from "./ManageProfile";
