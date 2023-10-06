d651bc96ee3aa1551ee2f23624be26a6add66d8c
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-10-06 11:44:54 -0300

    change: ProfileGroup styling works on other pages

--- src/components/ProfileInfo/ProfileGroups/ProfileGroups.tsx
+++ src/components/ProfileInfo/ProfileGroups/ProfileGroups.tsx
@@ -2,33 +2,33 @@ import { Link } from "react-router-dom";
 
 import type { Group } from "@/types/Group";
 
+import "./ProfileGroups.less";
+
 export type ProfileGroupsProps = {
     groups: Group[];
 };
 
 export function ProfileGroups(props: ProfileGroupsProps) {
     return (
-        <div className="groups card">
-            <div className="group-section">
-                <h1 className="groups-name">Meus grupos</h1>
-                <div className="items-wrapper">
-                    {
-                        props.groups.length <= 0
-                        ? <div className="empty-groups">Não participa de um grupo.</div>
-                        : <div className="show-items">
-                            {
-                                props.groups.map((group) => {
-                                    return group === undefined ? null : (
-                                        <Link to={`/group${group.path}`} className="group-item" title={group.name} key={group.id}>
-                                            {/* todo: set group url */}
-                                            <img src={group.image ?? undefined} alt="" />
-                                        </Link>
-                                    );
-                                })
-                            }
-                        </div>
-                    }
-                </div>
+        <div className="profile-groups-component">
+            <h1 className="groups-name">Meus grupos</h1>
+            <div className="items-wrapper">
+                {
+                    props.groups.length <= 0
+                    ? <div className="empty-groups">Não participa de um grupo.</div>
+                    : <div className="show-items">
+                        {
+                            props.groups.map((group) => {
+                                return group === undefined ? null : (
+                                    <Link to={`/group${group.path}`} className="group-item" title={group.name} key={group.id}>
+                                        {/* todo: set group url */}
+                                        <img src={group.image ?? undefined} alt="" />
+                                    </Link>
+                                );
+                            })
+                        }
+                    </div>
+                }
             </div>
         </div>
     );
