0320183001c1086389986ed58c26e649106f8c7a
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-10-06 13:42:47 -0300

    FIX #147: Componentes ProfileBio e ProfileGroup só funcionam na tela de perfil (#148)
    * change: sort imports
    
    * change: ProfileBio not using ProfileContext
    
    * change: ProfileBio CSS to LESS
    
    * change: ProfileBio styling works on other pages
    
    * removed unused functions getMemberSince and getFunctionGender
    * changed edit icon to white
    
    * change: ProfileGroup not using ProfileContext
    
    * change: ProfileGroup styling works on other pages
    
    * change: remove old .card css

--- src/components/ProfileInfo/ProfileBio/ProfileBio.tsx
+++ src/components/ProfileInfo/ProfileBio/ProfileBio.tsx
@@ -1,85 +1,50 @@
-import { MouseEventHandler, useContext, useEffect, useState } from 'react';
-import { ProfileContext } from '@/pages/Profile';
-import { getGenderName, getProfileImageUrl } from '@/utils/profileUtils';
-import { TypeLinkToBootstrapIcon } from '@/types/Link';
-import { ProfileImage } from '@/components/ProfileImage/ProfileImage';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ICON_EDIT_BLACK } from '@/utils/assets';
-import './ProfileBio.css'
+
+import { ProfileImage } from '@/components/ProfileImage/ProfileImage';
+import { getProfileImageUrl } from '@/utils/profileUtils';
+import { ICON_EDIT_WHITE } from '@/utils/assets';
 import UniversimeApi from '@/services/UniversimeApi';
 
+import type { Profile } from '@/types/Profile';
+import './ProfileBio.less';
+
 export type ProfileBioProps = {
-    onClickEdit: MouseEventHandler;
+    profile: Profile;
 };
 
 
-
-
 export function ProfileBio(props: ProfileBioProps) {
-    const profileContext = useContext(ProfileContext)
     const [contentCounter, setContentCounter] = useState(10)
 
     useEffect(() =>{
     UniversimeApi.Capacity.contentList()
     .then(res=>setContentCounter(res.body?.contents.length == undefined ? 0 : res.body.contents.length))
-    }, [profileContext?.profile.user.name])
+    }, [props.profile.user.name])
 
     return (
-        profileContext === null ? null :
-
-        <div className="bio card">
+        <div className="profile-bio-component card">
 
             <div className='profile-header'>
                 {
-                    profileContext.accessingLoggedUser ?
+                    props.profile.user.ownerOfSession ?
                         <Link className="edit-button" to="/manage-profile">
-                            <img src={ICON_EDIT_BLACK} alt="Editar" />
+                            <img src={ICON_EDIT_WHITE} alt="Editar" />
                         </Link>
                     : null
                 }
             </div>
 
             <div className="intro intro-section">
-                <ProfileImage className="image" imageUrl={getProfileImageUrl(profileContext.profile)} noImageColor="#505050" />
-                <h2 className="card-heading name">{ `${profileContext.profile.firstname} ${profileContext.profile.lastname}` }</h2>
+                <ProfileImage className="image" imageUrl={getProfileImageUrl(props.profile)} noImageColor="#505050" />
+                <h2 className="card-heading name">{ `${props.profile.firstname} ${props.profile.lastname}` }</h2>
                 {
-                    profileContext.profile.bio === null || profileContext.profile.bio.length === 0
+                    props.profile.bio === null || props.profile.bio.length === 0
                     ? <p style={{fontStyle: 'italic', textAlign: 'center'}}>Nenhuma bio</p>
-                    : <p style={{whiteSpace: 'break-spaces', textAlign: 'center'}}>{ profileContext.profile.bio }</p>
+                    : <p style={{whiteSpace: 'break-spaces', textAlign: 'center'}}>{ props.profile.bio }</p>
                 }
             </div>
-
-            {
-                profileContext.profileListData.links.length === 0 ? null :
-                <div className="links section">
-                    <h3 className="section-heading">Links Importantes</h3>
-                    <div className="link-wrapper">
-                        {
-                            profileContext.profileListData.links.map((link) => {
-                                return (<a key={link.id} href={link.url} target='_blank' className="link">
-                                    <i className={`icon bi-${TypeLinkToBootstrapIcon[link.typeLink]}`} style={{fontSize: "1.5rem", color: "black"}}></i>
-                                    {link.name}
-                                </a>);
-                            })
-                        }
-                    </div>
-                </div>
-            }
             <div className="content-count">Meus conteúdos: {contentCounter}</div>
         </div>
     );
-
-    function getMemberSince() {
-        const date = new Date(profileContext?.profile.creationDate ?? '')
-
-        const day = date.getDate().toLocaleString('pt-BR', { minimumIntegerDigits: 2, useGrouping: false });
-        const month = date.getMonth().toLocaleString('pt-BR', { minimumIntegerDigits: 2, useGrouping: false });
-        const year = date.getFullYear().toLocaleString('pt-BR', { minimumIntegerDigits: 2, useGrouping: false });
-
-        return `${day}/${month}/${year}`
-    }
-
-    function getFunctionGender() {
-        return `Gênero: ${getGenderName(profileContext?.profile.gender)}`
-    }
 }
