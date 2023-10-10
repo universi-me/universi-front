df41af60b2cfe61b7620a1d8c83245af5c729e2a
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-10-10 11:03:11 -0300

    change: remove old group components

--- src/pages/Group/index.ts
+++ src/pages/Group/index.ts
@@ -2,11 +2,7 @@ import { GroupPage } from "./Group"
 export default GroupPage;
 
 export * from "./Group"
-export * from "./GroupBanner/GroupBanner"
 export * from "./GroupIntro/GroupIntro"
-export * from "./GroupAbout/GroupAbout"
-export * from "./GroupSubGroups/GroupSubGroups"
-export * from "./GroupMembers/GroupMembers"
 export * from "./GroupTabs"
 export * from "./GroupTabs/GroupTabs"
 export * from "./GroupContext"
