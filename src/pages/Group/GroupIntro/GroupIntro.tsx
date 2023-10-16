814c4d1ca137b9efe0a7b86750464748e4ef23dc
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-10-16 12:31:58 -0300

    change: organization name on banner
    * don't render group name if it's the organization

--- src/pages/Group/GroupIntro/GroupIntro.tsx
+++ src/pages/Group/GroupIntro/GroupIntro.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 import { GroupContext } from "@/pages/Group";
 import { groupBannerUrl } from "@/utils/apiUtils";
@@ -7,6 +8,7 @@ import "./GroupIntro.less";
 
 export function GroupIntro() {
     const groupContext = useContext(GroupContext);
+    const organization = groupContext?.group.organization ?? undefined;
 
     return (
         groupContext === null ? null :
@@ -14,7 +16,16 @@ export function GroupIntro() {
         <div id="group-intro">
             <div id="banner-wrapper">
                 <img id="organization-banner" src={groupBannerUrl(groupContext.group)} />
-                <h3 id="group-name">{groupContext.group.name}</h3>
+                <h3 id="group-name">
+                    { organization
+                        ? <>
+                            <Link to={`/group${organization.path}`}>{organization.name}</Link>
+                            {" > "}
+                            {groupContext.group.name}
+                          </>
+                        : null
+                    }
+                </h3>
             </div>
         </div>
     );
