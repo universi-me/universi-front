1f89409bc487e7b81bea0cc876fcadf8d83698ff
710lucas <lucasperonicobarbotin@gmail.com>
2023-10-17 20:25:30 -0300

    Adicionando botão

--- src/pages/Group/GroupTabs/GroupTabs.tsx
+++ src/pages/Group/GroupTabs/GroupTabs.tsx
@@ -1,7 +1,10 @@
-import type { ReactElement } from "react";
+import { useContext, type ReactElement, useMemo, useState, useEffect } from "react";
 
-import { GroupContents, GroupGroups, GroupPeople } from "@/pages/Group";
+import { GroupContents, GroupContext, GroupGroups, GroupPageLoaderResponse, GroupPeople } from "@/pages/Group";
 import "./GroupTabs.less";
+import { useLoaderData } from "react-router-dom";
+import { Group } from "@/types/Group";
+import UniversimeApi from "@/services/UniversimeApi";
 
 export type AvailableTabs = "contents" | "files" | "groups" | "people";
 
@@ -16,9 +19,42 @@ export type GroupTabsProps = {
     changeTab: (tab: AvailableTabs) => any;
 };
 
-export function GroupTabs(props: GroupTabsProps) {
+export function GroupTabs(props: GroupTabsProps){
+
+    let page = useLoaderData() as GroupPageLoaderResponse;
+    const context = useContext(GroupContext);
+
+    const [joined, setJoined] = useState<boolean>(containsGroup())
+
+    function containsGroup(){
+        const userGroups : (Group | undefined)[] | undefined = page.loggedData?.groups;
+        if(!userGroups)
+            return false;
+
+        for (const value of userGroups){
+            if (context?.group.id.trim()  == value?.id.trim()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function join(){
+        context?.group.id != undefined ? UniversimeApi.Group.join({groupId: context.group.id}) : null;
+        setJoined(containsGroup())
+        location.reload()
+    }
+
+    function leave(){
+        context?.group.id != undefined ? UniversimeApi.Group.exit({groupId: context?.group.id}): null; // enviar mensagem que deu errado;
+        setJoined(containsGroup())
+        location.reload()
+    }
+
+
     return (
-        <nav id="group-tabs"> {
+        <nav id="group-tabs"> 
+        {
             TABS.map(t => {
                 return (
                     <button className="group-tab-button" value={t.value} key={t.value} onClick={_ => props.changeTab(t.value)} disabled={t.value === props.currentTab}>
@@ -26,7 +62,15 @@ export function GroupTabs(props: GroupTabsProps) {
                     </button>
                 );
             })
-        } </nav>
+        }
+        {
+            joined ? 
+            <button className="group-tab-button group-tab-participacao" onClick={leave}>Sair</button> 
+            :
+            <button className="group-tab-button group-tab-participacao" onClick={join}>Participar</button> 
+        }
+
+         </nav>
     );
 }
 
