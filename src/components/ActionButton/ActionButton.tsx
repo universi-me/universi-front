17d38da245bf73b75d64efca027cd637646f3f8e
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-11-14 14:27:35 -0300

    change: add div props to ActionButton

--- src/components/ActionButton/ActionButton.tsx
+++ src/components/ActionButton/ActionButton.tsx
@@ -1,14 +1,19 @@
+import { HTMLAttributes } from "react"
 import "./ActionButton.less"
 
 export interface ActionButtonProps{
     name : string
+    buttonProps?: HTMLAttributes<HTMLDivElement>;
 }
 
 export function ActionButton(props : ActionButtonProps){
+    const className = ["action-button-container", props.buttonProps?.className]
+        .filter(c => !!c && c.length > 0)
+        .join(" ");
 
 
     return(
-        <div className="action-button-container">
+        <div {...props.buttonProps} className={className}>
             <i className="bi bi-plus"></i>
             <div className="action-button-name">
                 {props.name}
