0e7de339774429be9181d8169ec6632fa79f700a
Lucas Perônico Barbotin <lucas.peronico@dcx.ufpb.br>
2023-11-14 12:22:27 -0300

    cor principal quando não há banner de grupo

--- src/pages/Group/GroupIntro/GroupIntro.tsx
+++ src/pages/Group/GroupIntro/GroupIntro.tsx
@@ -9,19 +9,25 @@ import "./GroupIntro.less";
 export function GroupIntro() {
     const groupContext = useContext(GroupContext);
     const organization = groupContext?.group.organization ?? undefined;
+    const banner = groupContext == null ? null : groupBannerUrl(groupContext.group);
 
     return (
         groupContext === null ? null :
 
         <div id="group-intro">
             <div id="banner-wrapper">
-                <img id="organization-banner" src={groupBannerUrl(groupContext.group)} />
+                {
+                    banner ?
+                    <>
+                        <img id="organization-banner" src={banner} />
+                    </>
+                    :
+                        <div id="organization-banner" style={{backgroundColor: "var(--primary-color)"}}></div>
+                }
                 <div className="image-overlay"></div>
                 <h3 id="group-name">
                     { organization
                         ? <>
-                            <Link to={`/group${organization.path}`}>{organization.name}</Link>
-                            {" > "}
                             {groupContext.group.name}
                           </>
                         : null
--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,8 +1,8 @@
 import { Group } from "@/types/Group";
 
 export function groupBannerUrl(group: Group) {
-    if (group.organization?.bannerImage)
-        group = group.organization;
+    if(!group.bannerImage)
+        return null
 
     return `${import.meta.env.VITE_UNIVERSIME_API}/group/banner/${group.id}`;
 }
