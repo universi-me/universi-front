3d82614042f6afa6f54dd7427c36328a45c70a75
Douglas Sebastian <kagnanii.miyo@gmail.com>
2023-11-15 22:27:18 -0300

    change: insert url and type on material

--- src/pages/Group/GroupTabs/GroupContents/ManageMaterial/ManageMaterial.tsx
+++ src/pages/Group/GroupTabs/GroupContents/ManageMaterial/ManageMaterial.tsx
@@ -1,10 +1,12 @@
 import { useContext, useEffect, useState } from "react";
 import Select from 'react-select';
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
 import { CATEGORY_SELECT_STYLES, GroupContext } from "@/pages/Group";
 import UniversimeApi from "@/services/UniversimeApi";
 import { UniversiModal } from "@/components/UniversiModal";
 import { setStateAsValue } from "@/utils/tsxUtils";
+import { type OptionInMenu, renderOption } from "@/utils/dropdownMenuUtils";
 
 import type { ContentCreate_ResponseDTO, ContentEdit_ResponseDTO } from "@/services/UniversimeApi/Capacity";
 import type { Category, ContentType } from "@/types/Capacity";
@@ -16,8 +18,9 @@ export type ManageMaterialProps = {
 
 const MAX_TITLE_LENGTH = 50;
 const MAX_DESC_LENGTH = 150;
+const MAX_URL_LENGTH = 100;
 
-export function ManageMaterial(props: ManageMaterialProps) {
+export function ManageMaterial(props: Readonly<ManageMaterialProps>) {
     const context = useContext(GroupContext);
 
     const [title, setTitle] = useState<string>(context?.editMaterial?.title ?? "");
@@ -59,6 +62,18 @@ export function ManageMaterial(props: ManageMaterialProps) {
     const canSave = (title.length > 0) && (description.length > 0) && (url.length > 0);
     const isNewMaterial = context.editMaterial === null;
 
+    const contentTypeIcon = getContentTypeIcon(type);
+
+    const MATERIAL_TYPE_OPTIONS: [OptionInMenu<ContentType>, ContentType][] = AVAILABLE_MATERIAL_TYPES
+        .map(t => {
+            return [{
+                text: getContentTypeOptionText(t[0], t[1]),
+                className: "type-dropdown-option",
+                onSelect(data){ setType(data); },
+            }, t[0]]
+        });
+
+
     return <UniversiModal>
         <div id="manage-material">
 
@@ -67,43 +82,68 @@ export function ManageMaterial(props: ManageMaterialProps) {
                 <h1 className="title">{ isNewMaterial ? "Criar" : "Editar" } material</h1>
             </div>
 
-            <fieldset>
-                <legend>
-                    Título do Material
-                    <div className="char-counter">
-                        {title.length} / {MAX_TITLE_LENGTH}
-                    </div>
-                </legend>
-                <input className="field-input" type="text" defaultValue={context.editMaterial?.title} onChange={setStateAsValue(setTitle)} maxLength={MAX_TITLE_LENGTH}/>
-            </fieldset>
+            <div className="manage-material fields">
+                <fieldset>
+                    <legend>
+                        Título do Material
+                        <div className="char-counter">
+                            {title.length} / {MAX_TITLE_LENGTH}
+                        </div>
+                    </legend>
+                    <input className="field-input" type="text" defaultValue={context.editMaterial?.title} onChange={setStateAsValue(setTitle)} maxLength={MAX_TITLE_LENGTH}/>
+                </fieldset>
 
-            <fieldset>
-                <legend>
-                    Descrição
-                    <div className="char-counter">
-                        {description.length} / {MAX_DESC_LENGTH}
-                    </div>
-                </legend>
-                <textarea className="field-input" defaultValue={context.editMaterial?.description ?? undefined} onChange={setStateAsValue(setDescription)} maxLength={MAX_DESC_LENGTH} />
-            </fieldset>
+                <fieldset>
+                    <legend>
+                        Descrição
+                        <div className="char-counter">
+                            {description.length} / {MAX_DESC_LENGTH}
+                        </div>
+                    </legend>
+                    <textarea className="field-input" defaultValue={context.editMaterial?.description ?? undefined} onChange={setStateAsValue(setDescription)} maxLength={MAX_DESC_LENGTH} />
+                </fieldset>
 
-            <fieldset>
-                <legend>Categorias</legend>
-                <Select placeholder="Selecionar categorias..." className="category-select" isMulti options={availableCategories}
-                    onChange={(value) => {setCategoriesIds(value.map(v => v.id))}}
-                    defaultValue={ availableCategories.filter(c => context.editMaterial?.categories.map(c => c.id).includes(c.id)) } noOptionsMessage={()=>"Categoria Não Encontrada"}
-                    getOptionLabel={c => c.name} getOptionValue={c => c.id} classNamePrefix="category-item" styles={CATEGORY_SELECT_STYLES}
-                />
-            </fieldset>
+                <section className="url-wrapper">
+                    <fieldset className="type-fieldset">
+                        <legend>Tipo</legend>
+                        <DropdownMenu.Root>
+                            <DropdownMenu.Trigger asChild>
+                                <img className="material-type-trigger" src={`/assets/imgs/${contentTypeIcon}.png`} />
+                            </DropdownMenu.Trigger>
+
+                            <DropdownMenu.Content className="type-dropdown-menu" side="top">
+                                { MATERIAL_TYPE_OPTIONS.map(([def, data]) => renderOption(data, def)) }
+                            </DropdownMenu.Content>
+                        </DropdownMenu.Root>
+                    </fieldset>
+
+                    <fieldset className="url-fieldset">
+                        <legend>
+                            Link de acesso
+                            <div className="char-counter">{url.length}/{MAX_URL_LENGTH}</div>
+                        </legend>
+                        <input className="field-input" type="url" defaultValue={context.editMaterial?.url} onChange={setStateAsValue(setUrl)} maxLength={MAX_URL_LENGTH} />
+                    </fieldset>
+                </section>
+
+                <fieldset>
+                    <legend>Categorias</legend>
+                    <Select placeholder="Selecionar categorias..." className="category-select" isMulti options={availableCategories}
+                        onChange={(value) => {setCategoriesIds(value.map(v => v.id))}}
+                        defaultValue={ availableCategories.filter(c => context.editMaterial?.categories.map(c => c.id).includes(c.id)) } noOptionsMessage={()=>"Categoria Não Encontrada"}
+                        getOptionLabel={c => c.name} getOptionValue={c => c.id} classNamePrefix="category-item" styles={CATEGORY_SELECT_STYLES}
+                    />
+                </fieldset>
 
 
-            <section className="operation-buttons">
-                <button type="button" className="cancel-button" onClick={() => {context.setEditMaterial(undefined)}}>Cancelar</button>
-                <button type="button" className="submit-button" onClick={handleSaveMaterial} disabled={!canSave} title={canSave ? undefined : "Preencha os dados antes de salvar"}>
-                    <i className="bi bi-check-circle-fill" />
-                    { isNewMaterial ? "Criar" : "Salvar" }
-                </button>
-            </section>
+                <section className="operation-buttons">
+                    <button type="button" className="cancel-button" onClick={() => {context.setEditMaterial(undefined)}}>Cancelar</button>
+                    <button type="button" className="submit-button" onClick={handleSaveMaterial} disabled={!canSave} title={canSave ? undefined : "Preencha os dados antes de salvar"}>
+                        <i className="bi bi-check-circle-fill" />
+                        { isNewMaterial ? "Criar" : "Salvar" }
+                    </button>
+                </section>
+            </div>
         </div>
     </UniversiModal>
 
@@ -138,3 +178,24 @@ export function ManageMaterial(props: ManageMaterialProps) {
         }
     }
 }
+
+function getContentTypeIcon(type: ContentType) {
+    return type === "FILE" || type === "FOLDER" ? "file"
+        : type === "LINK" ? "link"
+        : type === "VIDEO" ? "video"
+        : "link";
+}
+
+function getContentTypeOptionText(type: ContentType, title: string) {
+    return <img
+        className="material-type-icon"
+        src={`/assets/imgs/${getContentTypeIcon(type)}.png`}
+        title={title}
+    />
+}
+
+const AVAILABLE_MATERIAL_TYPES: [ContentType, string][] = [
+    ["LINK", "Link"],
+    ["VIDEO", "Vídeo"],
+    ["FILE", "Arquivo"],
+];
