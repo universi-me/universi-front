f243236d00ad641357b92e5e22591f0fca9fdec5
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-11-28 11:17:46 -0300

    change: create ProfileClass implementing Profile

--- src/types/Profile.ts
+++ src/types/Profile.ts
@@ -12,3 +12,16 @@ export type Profile = {
     bio:          string | null;
     creationDate: string;
 }
+
+export class ProfileClass implements Profile {
+    constructor(private profile: Profile) {}
+
+    get id() { return this.profile.id }
+    get user() { return this.profile.user }
+    get firstname() { return this.profile.firstname }
+    get lastname() { return this.profile.lastname }
+    get bio() { return this.profile.bio }
+    get gender() { return this.profile.gender }
+    get image() { return this.profile.image }
+    get creationDate() { return this.profile.creationDate }
+}
