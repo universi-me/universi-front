8ebf492e585f99a186d4e8d2d14fe49de335383f
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-11-28 13:38:03 -0300

    change: profileUtils functions as ProfileClass Methods

--- src/types/Profile.ts
+++ src/types/Profile.ts
@@ -2,6 +2,11 @@ import type { User } from "@/types/User";
 import { type Nullable } from "@/types/utils";
 
 export type Gender = "M" | "F" | "O";
+export const GenderOptions: {[k in Gender]: string} = {
+    M: "Masculino",
+    F: "Feminino",
+    O: "Outro",
+};
 
 export type Profile = {
     id:           string;
@@ -17,6 +22,70 @@ export type Profile = {
 export class ProfileClass implements Profile {
     constructor(private profile: Profile) {}
 
+    /**
+     * Builds the full name of the profile.
+     *
+     * @returns {(string | null)} The concatenation of the first and last names, with a space in between
+     * or `null` if both are null.
+     */
+    get fullname(): Nullable<string> {
+        const hasFirst = this.firstname && this.firstname.length > 0;
+        const hasLast = this.lastname && this.lastname.length > 0;
+
+        if (!hasFirst && !hasLast)
+            return null;
+
+        return ( this.firstname ?? "" )
+            + ( hasFirst && hasLast ? " " : "" )
+            + ( this.lastname ?? "" );
+    }
+
+    /**
+     * User readable gender name, instead of the API value.
+     */
+    get genderName() {
+        if (this.gender)
+            return GenderOptions[this.gender];
+
+        // todo: use a constant with this value
+        return "Não informado";
+    }
+
+    /**
+     * Image URL ready to be used on an `<img>`.
+     */
+    get imageUrl() {
+        if (this.image === null)
+            return null;
+
+        return import.meta.env.VITE_UNIVERSIME_API + "/profile/image/" + this.id;
+    }
+
+    /**
+     * Separates a full name into a first name and a last name.
+     *
+     * @param {string} fullname The full name to be separated.
+     * @returns {[string, string]} An 2 element array, where the first element is
+     * the first name and the second is the last name or `undefined`, if `fullname`
+     * doesn't have a last name.
+     */
+    public static separateFullname(fullname: string): [string, string | undefined] {
+        fullname = fullname.trim();
+        const spaceIndex = fullname.indexOf(" ");
+
+        if (fullname.length === 0 || spaceIndex < 0)
+            return [fullname, undefined];
+
+        const firstname = fullname.slice(0, spaceIndex);
+        const lastname = fullname.slice(spaceIndex + 1);
+
+        return [
+            firstname,
+            lastname,
+        ];
+    }
+
+    /* Profile type getters and setters */
     get id() { return this.profile.id }
     set id(id: string) { this.profile.id = id }
 
