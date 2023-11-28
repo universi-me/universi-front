94b9b52528e818f88c499ce6c9513f88b2c35b73
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2023-11-28 11:23:11 -0300

    change: ProfileClass setters

--- src/types/Profile.ts
+++ src/types/Profile.ts
@@ -1,4 +1,5 @@
 import type { User } from "@/types/User";
+import { type Nullable } from "@/types/utils";
 
 export type Gender = "M" | "F" | "O";
 
@@ -17,11 +18,25 @@ export class ProfileClass implements Profile {
     constructor(private profile: Profile) {}
 
     get id() { return this.profile.id }
+    set id(id: string) { this.profile.id = id }
+
     get user() { return this.profile.user }
+    set user(user: User) { this.profile.user = user }
+
     get firstname() { return this.profile.firstname }
+    set firstname(firstname: Nullable<string>) { this.profile.firstname = firstname }
+
     get lastname() { return this.profile.lastname }
+    set lastname(lastname: Nullable<string>) { this.profile.lastname = lastname }
+
     get bio() { return this.profile.bio }
+    set bio(bio: Nullable<string>) { this.profile.bio = bio }
+
     get gender() { return this.profile.gender }
+    set gender(gender: Nullable<Gender>) { this.profile.gender = gender }
+
     get image() { return this.profile.image }
+    set image(image: Nullable<string>) { this.profile.image = image }
+
     get creationDate() { return this.profile.creationDate }
 }
