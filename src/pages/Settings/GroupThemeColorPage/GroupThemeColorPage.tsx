db7fccfade656ee51a6ecc0a1e244d0de2c17e51
kassioL2L <kassio_leite2@hotmail.com>
2023-11-28 15:24:01 -0300

    refatorando

--- src/pages/Settings/GroupThemeColorPage/GroupThemeColorPage.tsx
+++ src/pages/Settings/GroupThemeColorPage/GroupThemeColorPage.tsx
@@ -1,90 +1,131 @@
-import React, { useEffect, useReducer } from "react";
+import { useReducer } from "react";
 import * as SwalUtils from "@/utils/sweetalertUtils";
 import { ActionButton } from "@/components/ActionButton/ActionButton";
 import { SettingsTitle, SettingsDescription } from "@/pages/Settings";
 import UniversimeApi from "@/services/UniversimeApi";
+import type { GroupThemeEdit } from "@/types/Group";
 import "./GroupThemeColor.less";
 
 const themeColorMappings = {
   themeId1: {
     id: "themeId1",
     name: "Tema 1",
-    "--primary-color": "#0091B9",
-    "--secondary-color": "#934588",
-    "--tertiary-color": "#FFFFFF",
-    "--background-color": "#F5F4F4",
-    "--card-background-color": "#D9D9D9",
-    "--card-item-color": "#F3F3F3",
-    "--font-color-v1": "#FFFFFF",
-    "--font-color-v2": "#191919",
-    "--font-color-v3": "#8A8A8A",
-    "--font-color-v4": "#7D7EAE",
-    "--font-color-v5": "#F5F5F5",
-    "--font-color-v6": "#4E4E4E",
-    "--font-disabled-color": "#6F6F6F",
-    "--forms-color": "#E0E0E0",
-    "--skills-1-color": "#934588",
-    "--wave-color": "#9294CC",
-    "--button-yellow-hover-color": "#d3a61e",
-    "--button-hover-color": "#FFFFFF", 
-    "--alert-color": "#CC615B",
-    "--success-color": "#35BD00",
-    "--wrong-invalid-color": "#B33B3B",
-    "--rank-color": "#6E70AF",
+    primaryColor: "#0091B9",
+    secondaryColor: "#934588",
+    tertiaryColor: "#FFFFFF",
+    backgroundColor: "#F5F4F4",
+    cardBackgroundColor: "#D9D9D9",
+    cardItemColor: "#F3F3F3",
+    fontColorV1: "#FFFFFF",
+    fontColorV2: "#191919",
+    fontColorV3: "#8A8A8A",
+    fontColorV4: "#7D7EAE",
+    fontColorV5: "#F5F5F5",
+    fontColorV6: "#4E4E4E",
+    fontDisabledColor: "#6F6F6F",
+    formsColor: "#E0E0E0",
+    skills1Color: "#934588",
+    waveColor: "#9294CC",
+    buttonYellowHoverColor: "#d3a61e",
+    buttonHoverColor: "#FFFFFF",
+    alertColor: "#CC615B",
+    successColor: "#35BD00",
+    wrongInvalidColor: "#B33B3B",
+    rankColor: "#6E70AF",
   },
-  
   themeId2: {
     id: "themeId2",
     name: "Tema 2",
-    "--primary-color": "#FF5733",
-    "--secondary-color": "#5E35B1",
-    "--tertiary-color": "#00C853",
-    "--background-color": "#E6EE9C",
-    "--card-background-color": "#FFCCBC",
-    "--card-item-color": "#B2DFDB",
-    "--font-color-v1": "#FFD54F",
-    "--font-color-v2": "#303F9F",
-    "--font-color-v3": "#689F38",
-    "--font-color-v4": "#FF4081",
-    "--font-color-v5": "#212121",
-    "--font-color-v6": "#64FFDA",
-    "--font-disabled-color": "#CFD8DC",
-    "--forms-color": "#FF6F00",
-    "--skills-1-color": "#5E35B1",
-    "--wave-color": "#81C784",
-    "--button-yellow-hover-color": "#EF5350",
-    "--button-hover-color": "#00C853",
-    "--alert-color": "#4FC3F7",
-    "--success-color": "#64FFDA",
-    "--wrong-invalid-color": "#FF5252",
-    "--rank-color": "#00BFA5",
+    primaryColor: "#FF5733",
+    secondaryColor: "#5E35B1",
+    tertiaryColor: "#00C853",
+    backgroundColor: "#E6EE9C",
+    cardBackgroundColor: "#FFCCBC",
+    cardItemColor: "#B2DFDB",
+    fontColorV1: "#FFD54F",
+    fontColorV2: "#303F9F",
+    fontColorV3: "#689F38",
+    fontColorV4: "#FF4081",
+    fontColorV5: "#212121",
+    fontColorV6: "#64FFDA",
+    fontDisabledColor: "#CFD8DC",
+    formsColor: "#FF6F00",
+    skills1Color: "#5E35B1",
+    waveColor: "#81C784",
+    buttonYellowHoverColor: "#EF5350",
+    buttonHoverColor: "#00C853",
+    alertColor: "#4FC3F7",
+    successColor: "#64FFDA",
+    wrongInvalidColor: "#FF5252",
+    rankColor: "#00BFA5",
   },
   themeId3: {
     id: "themeId3",
     name: "Tema 3",
-    "--primary-color": "#FFEB3B",
-    "--secondary-color": "#03A9F4",
-    "--tertiary-color": "#FFC107",
-    "--background-color": "#B2EBF2",
-    "--card-background-color": "#FFD180",
-    "--card-item-color": "#FF80AB",
-    "--font-color-v1": "#880E4F",
-    "--font-color-v2": "#01579B",
-    "--font-color-v3": "#FF8F00",
-    "--font-color-v4": "#004D40",
-    "--font-color-v5": "#F57F17",
-    "--font-color-v6": "#3E2723",
-    "--font-disabled-color": "#757575",
-    "--forms-color": "#E64A19",
-    "--skills-1-color": "#03A9F4",
-    "--wave-color": "#CDDC39",
-    "--button-yellow-hover-color": "#FF5722",
-    "--button-hover-color": "#FFC107",
-    "--alert-color": "#536DFE",
-    "--success-color": "#8BC34A",
-    "--wrong-invalid-color": "#FFD600",
-    "--rank-color": "#795548",
+    primaryColor: "#FFEB3B",
+    secondaryColor: "#03A9F4",
+    tertiaryColor: "#FFC107",
+    backgroundColor: "#B2EBF2",
+    cardBackgroundColor: "#FFD180",
+    cardItemColor: "#FF80AB",
+    fontColorV1: "#880E4F",
+    fontColorV2: "#01579B",
+    fontColorV3: "#FF8F00",
+    fontColorV4: "#004D40",
+    fontColorV5: "#F57F17",
+    fontColorV6: "#3E2723",
+    fontDisabledColor: "#757575",
+    formsColor: "#E64A19",
+    skills1Color: "#03A9F4",
+    waveColor: "#CDDC39",
+    buttonYellowHoverColor: "#FF5722",
+    buttonHoverColor: "#FFC107",
+    alertColor: "#536DFE",
+    successColor: "#8BC34A",
+    wrongInvalidColor: "#FFD600",
+    rankColor: "#795548",
   },
+};
+
+interface ThemeColorItemProps {
+  theme: GroupThemeEdit;
+  isSelected: boolean;
+  onClick: (theme: GroupThemeEdit) => void;
+}
+
+function ThemeColorItem({ theme, isSelected, onClick }: ThemeColorItemProps) {
+  return (
+    <div
+      className={`theme-color-item ${isSelected ? "selected" : ""}`}
+      style={{ backgroundColor: theme.primaryColor }}
+      onClick={() => onClick(theme)}
+    ></div>
+  );
+}
+
+function themeReducer(state: GroupThemeEdit | null, action: { type: "SELECT"; theme: GroupThemeEdit }) {
+  if (action.type === "SELECT") {
+    return action.theme;
+  }
+  return state;
+}
+
+const showErrorModal = (title: string, text: string) => {
+  SwalUtils.fireModal({
+    title,
+    text,
+    showConfirmButton: true,
+    confirmButtonText: "OK",
+  });
+};
+
+const showSuccessModal = (title: string, text: string) => {
+  SwalUtils.fireModal({
+    title,
+    text,
+    showConfirmButton: true,
+    confirmButtonText: "OK",
+  });
 };
 
 export function GroupThemeColorPage() {
@@ -92,47 +133,22 @@ export function GroupThemeColorPage() {
 
   const saveChanges = async () => {
     if (!selectedTheme) {
-      SwalUtils.fireModal({
-        title: "Erro ao salvar alterações",
-        text: "Selecione um tema antes de salvar.",
-        showConfirmButton: true,
-        confirmButtonText: "OK",
-      });
+      showErrorModal("Erro ao salvar alterações", "Selecione um tema antes de salvar.");
       return;
     }
 
     try {
-      const groupId = "groupId"; 
       const themeMapping = themeColorMappings[selectedTheme.id];
-      
-      // Ajuste o método editTheme conforme necessário para se adequar ao seu código
+
       await UniversimeApi.Group.editTheme({
-        groupId,
         ...themeMapping,
       });
 
-      SwalUtils.fireModal({
-        title: "Alterações salvas!",
-        text: `O tema foi atualizado para ${selectedTheme.name}.`,
-        showConfirmButton: true,
-        confirmButtonText: "OK",
-      });
+      showSuccessModal("Alterações salvas!", `O tema foi atualizado para ${selectedTheme.name}.`);
     } catch (error) {
-      SwalUtils.fireModal({
-        title: "Erro ao salvar alterações",
-        text: "Ocorreu um erro ao salvar as alterações do tema. Por favor, tente novamente.",
-        showConfirmButton: true,
-        confirmButtonText: "OK",
-      });
+      showErrorModal("Erro ao salvar alterações", "Ocorreu um erro ao salvar as alterações do tema. Por favor, tente novamente.");
     }
   };
-
-  function themeReducer(state, action) {
-    if (action.type === "SELECT") {
-      return action.theme;
-    }
-    return state;
-  }
 
   return (
     <div id="theme-color-settings">
@@ -141,12 +157,12 @@ export function GroupThemeColorPage() {
 
       <div className="theme-color-list">
         {Object.values(themeColorMappings).map((theme) => (
-          <div
+          <ThemeColorItem
             key={theme.id}
-            className={`theme-color-item ${selectedTheme?.id === theme.id ? "selected" : ""}`}
-            style={{ backgroundColor: theme["--primary-color"] }}
-            onClick={() => themeDispatch({ type: "SELECT", theme })}
-          ></div>
+            theme={theme}
+            isSelected={selectedTheme?.id === theme.id}
+            onClick={(selectedTheme) => themeDispatch({ type: "SELECT", theme })}
+          />
         ))}
       </div>
 
