4706d236f1c4919742ac219c31c4cf0b9e5a13e8
kassioL2L <kassio_leite2@hotmail.com>
2023-11-29 17:42:14 -0300

    quase la

--- src/pages/Settings/GroupThemeColorPage/GroupThemeColorPage.tsx
+++ src/pages/Settings/GroupThemeColorPage/GroupThemeColorPage.tsx
@@ -124,6 +124,37 @@ const showSuccessModal = (title: string, text: string) => {
   });
 };
 
+const applyThemeStyles = (themeMapping : GroupThemeEdit) => {
+  const styleProperties = {
+    '--primary-color': themeMapping.primaryColor,
+    '--secondary-color': themeMapping.secondaryColor,
+    '--tertiary-color': themeMapping.tertiaryColor,
+    '--background-color': themeMapping.backgroundColor,
+    '--card-background-color': themeMapping.cardBackgroundColor,
+    '--card-item-color': themeMapping.cardItemColor,
+    '--font-color-v1': themeMapping.fontColorV1,
+    '--font-color-v2': themeMapping.fontColorV2,
+    '--font-color-v3': themeMapping.fontColorV3,
+    '--font-color-v4': themeMapping.fontColorV4,
+    '--font-color-v5': themeMapping.fontColorV5,
+    '--font-color-v6': themeMapping.fontColorV6,
+    '--font-disabled-color': themeMapping.fontDisabledColor,
+    '--forms-color': themeMapping.formsColor,
+    '--skills-1-color': themeMapping.skills1Color,
+    '--wave-color': themeMapping.waveColor,
+    '--button-yellow-hover-color': themeMapping.buttonYellowHoverColor,
+    '--button-hover-color': themeMapping.buttonHoverColor,
+    '--alert-color': themeMapping.alertColor,
+    '--success-color': themeMapping.successColor,
+    '--wrong-invalid-color': themeMapping.wrongInvalidColor,
+    '--rank-color': themeMapping.rankColor,
+  };
+
+  for (const [property, value] of Object.entries(styleProperties)) {
+    document.documentElement.style.setProperty(property, value);
+  }
+};
+
 export function GroupThemeColorPage() {
   const [organizationId, setOrganizationId] = useState<string | null>(null);
   const [selectedTheme, themeDispatch] = useReducer(themeReducer, null);
@@ -156,7 +187,7 @@ export function GroupThemeColorPage() {
         groupId: organizationId,
         ...themeMapping,
       });
-
+      applyThemeStyles(themeMapping);
       showSuccessModal("Alterações salvas!", "O tema foi atualizado com sucesso");
     } catch {
       showErrorModal(
