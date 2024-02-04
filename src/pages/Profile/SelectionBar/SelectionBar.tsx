b958fd5439b429c0e6816427201a860e74c627ab
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2024-02-04 13:56:37 -0300

    change: "assigned by me" tab

--- src/pages/Profile/SelectionBar/SelectionBar.tsx
+++ src/pages/Profile/SelectionBar/SelectionBar.tsx
@@ -1,26 +1,33 @@
-import {useContext, useEffect, useState} from "react"
+import {useContext, useEffect, useMemo, useState} from "react"
 
 import "./SelectionBar.css"
-import { ProfileContext, ProfileContentListing, ProfileGroupListing, ProfileCurriculum } from "@/pages/Profile";
+import { ProfileContext, ProfileContentListing, ProfileGroupListing, ProfileCurriculum, ProfileAssignedByMe, ProfileContextType } from "@/pages/Profile";
 import { makeClassName } from "@/utils/tsxUtils";
 
 const INITIAL_TAB: AvailableTabs = "groups";
 export function SelectionBar(){
     const profileContext = useContext(ProfileContext);
     const [currentTab, setCurrentTab] = useState<AvailableTabs>(INITIAL_TAB);
-    const renderTabs = TABS.length > 1;
 
     useEffect(() => {
         setCurrentTab(INITIAL_TAB);
     }, [profileContext?.profile.user.name]);
 
+    const renderedTabs = useMemo(() => {
+        if (!profileContext) return [];
+
+        return TABS.filter(t => !t.visibleIf || t.visibleIf(profileContext));
+    }, [profileContext]);
+
+    const shouldRenderTabSelector = renderedTabs.length > 1;
+
     return(
         <>
             {
-                !renderTabs ? null : 
+                shouldRenderTabSelector &&
                 <div className="selection-bar">
                     {
-                        TABS.map((tab) => {
+                        renderedTabs.map((tab) => {
                             return <div key={tab.value} className={makeClassName("select-element", currentTab === tab.value && "current-tab")} onClick={() => setCurrentTab(tab.value)}>{tab.name}</div>
                         })
                     }
@@ -31,11 +38,12 @@ export function SelectionBar(){
     )
 }
 
-export type AvailableTabs = "groups" | "contents" | "curriculum";
+export type AvailableTabs = "groups" | "contents" | "curriculum" | "assigned-by-me";
 
 type TabDefinition = {
     name: string;
     value: AvailableTabs;
+    visibleIf?(c: NonNullable<ProfileContextType>): boolean;
 };
 
 const TABS: TabDefinition[] = [
@@ -51,6 +59,13 @@ const TABS: TabDefinition[] = [
         name: "Conteúdos",
         value: "contents",
     },
+    {
+        name: "Atribuições",
+        value: "assigned-by-me",
+        visibleIf(c) {
+            return c.profileListData.assignedByMe.length > 0;
+        },
+    }
 ];
 
 function renderTab(tab: AvailableTabs) {
@@ -58,6 +73,7 @@ function renderTab(tab: AvailableTabs) {
         case "contents": return <ProfileContentListing />;
         case "groups":   return <ProfileGroupListing />;
         case "curriculum": return <ProfileCurriculum/>;
+        case "assigned-by-me": return <ProfileAssignedByMe />
         default: return null;
     }
 }
