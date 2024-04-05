360b90271b6ff947c3f426d2a967b7fa9301b19a
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2024-04-05 08:31:24 -0300

    change: use const on SinginForm.tsx

--- src/pages/singin/SinginForm.tsx
+++ src/pages/singin/SinginForm.tsx
@@ -40,11 +40,11 @@ export default function SinginForm() {
     setShowPassword(!showPassword);
   };
 
-  let organizationEnv = (((auth.organization??{} as any).groupSettings??{} as any).environment??{} as any);
-  let SIGNUP_ENABLED = organizationEnv.signup_enabled ?? true;
-  let ENABLE_GOOGLE_LOGIN = organizationEnv.login_google_enabled ?? (import.meta.env.VITE_ENABLE_GOOGLE_LOGIN === "true" || import.meta.env.VITE_ENABLE_GOOGLE_LOGIN === "1");
-  let ENABLE_RECAPTCHA = organizationEnv.recaptcha_enabled ?? (import.meta.env.VITE_ENABLE_RECAPTCHA === "true" || import.meta.env.VITE_ENABLE_RECAPTCHA === "1");
-  let RECAPTCHA_SITE_KEY = organizationEnv.recaptcha_site_key ?? import.meta.env.VITE_RECAPTCHA_SITE_KEY;
+  const organizationEnv = (((auth.organization??{} as any).groupSettings??{} as any).environment??{} as any);
+  const SIGNUP_ENABLED = organizationEnv.signup_enabled ?? true;
+  const ENABLE_GOOGLE_LOGIN = organizationEnv.login_google_enabled ?? (import.meta.env.VITE_ENABLE_GOOGLE_LOGIN === "true" || import.meta.env.VITE_ENABLE_GOOGLE_LOGIN === "1");
+  const ENABLE_RECAPTCHA = organizationEnv.recaptcha_enabled ?? (import.meta.env.VITE_ENABLE_RECAPTCHA === "true" || import.meta.env.VITE_ENABLE_RECAPTCHA === "1");
+  const RECAPTCHA_SITE_KEY = organizationEnv.recaptcha_site_key ?? import.meta.env.VITE_RECAPTCHA_SITE_KEY;
   
   return (
   <>
