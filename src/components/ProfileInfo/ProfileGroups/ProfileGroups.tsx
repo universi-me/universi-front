c1f863c097fadd1ca0481546b3d5fab23dfc0134
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2024-07-24 11:28:59 -0300

    refactor: clean code a bit

--- src/components/ProfileInfo/ProfileGroups/ProfileGroups.tsx
+++ src/components/ProfileInfo/ProfileGroups/ProfileGroups.tsx
@@ -9,33 +9,34 @@ export type ProfileGroupsProps = {
     groups: Group[];
 };
 
-export function ProfileGroups(props: ProfileGroupsProps) {
+export function ProfileGroups(props: Readonly<ProfileGroupsProps>) {
+    const { groups } = props;
 
-
-    return (
-        <div className="profile-groups-component">
-            <div className="header-container">
-                <img src="/assets/imgs/group2.png" className="group-background-image"></img>
-                <h1 className="groups-name">Meus grupos</h1>
-            </div>
-            <div className="items-wrapper">
-                {
-                    props.groups.length <= 0
-                    ? <div className="empty-groups">Não participa de um grupo.</div>
-                    : <div className="show-items">
-                        {
-                            props.groups.map((group) => {
-                                return group === undefined ? null : (
-                                    <Link to={`/group${group.path}`} className="group-item" title={group.name} key={group.id}>
-                                        {/* todo: set group url */}
-                                        <img src={groupImageUrl(group)} alt="" />
-                                    </Link>
-                                );
-                            })
-                        }
-                    </div>
-                }
-            </div>
+    return <div className="profile-groups-component">
+        <div className="header-container">
+            <img src="/assets/imgs/group2.png" className="group-background-image" />
+            <h1 className="groups-name">Meus grupos</h1>
         </div>
-    );
+
+        <div className="items-wrapper">
+            { groups.length <= 0
+                ? <div className="empty-groups">Não participa de um grupo.</div>
+                : <div className="show-items">
+                    { groups.map(g => <GroupIcon group={g} key={g.id} />) }
+                  </div>
+            }
+        </div>
+    </div>;
+}
+
+type GroupIconProps = {
+    group: Group;
+};
+
+function GroupIcon(props: Readonly<GroupIconProps>) {
+    const { group } = props;
+
+    return <Link to={`/group${group.path}`} className="group-item" title={group.name} key={group.id}>
+        <img src={groupImageUrl(group)} alt="" />
+    </Link>
 }
