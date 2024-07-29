877025f6086071b864d8d1fe2005ae5cd539c7fc
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2024-07-29 12:21:52 -0300

    chande: use div instead of alt to prevent image error icon

--- src/components/UniversiHeader/components/OrganizationLogo.tsx
+++ src/components/UniversiHeader/components/OrganizationLogo.tsx
@@ -1,24 +1,36 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 import AuthContext from "@/contexts/Auth";
-import { groupHeaderUrl } from "@/utils/apiUtils";
 
 export function OrganizationLogo() {
     const authContext = useContext(AuthContext);
+    const [renderLogoImage, setRenderLogoImage] = useState(true);
+
+    useEffect(() => {
+        setRenderLogoImage(true);
+    }, [authContext.organization, authContext.profile]);
 
     const wrapperAttributes = {
         id: "header-logo",
 
-        children: <img
-            src={groupHeaderUrl(authContext.organization)}
-            className="organization-logo"
-            alt={authContext.organization.name}
-            title={authContext.organization.name}
-        />
+        children: renderLogoImage
+            ? <img
+                src={`${import.meta.env.VITE_UNIVERSIME_API}/group/current-organization/header`}
+                onError={toggleRenderImage}
+                className="organization-logo"
+                title={authContext.organization.name}
+            />
+            : <div>
+                { authContext.organization.name }
+            </div>
     };
 
     return authContext.profile
         ? <Link {...wrapperAttributes} to={`/group${authContext.organization.path}`} />
         : <div {...wrapperAttributes}/>
+
+    function toggleRenderImage() {
+        setRenderLogoImage(b => !b);
+    }
 }
