bd85dfecc89eefd483dadc2a7334aa29314e9585
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2024-07-29 11:40:09 -0300

    fix: display name as img alt

--- src/components/UniversiHeader/components/OrganizationLogo.tsx
+++ src/components/UniversiHeader/components/OrganizationLogo.tsx
@@ -1,4 +1,4 @@
-import { createRef, useContext } from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import AuthContext from "@/contexts/Auth";
@@ -6,28 +6,19 @@ import { groupHeaderUrl } from "@/utils/apiUtils";
 
 export function OrganizationLogo() {
     const authContext = useContext(AuthContext);
-    const imageLogoRef = createRef<HTMLImageElement>();
-
-    function imgLogoLoadError() {
-        if (!imageLogoRef.current) return;
-
-        imageLogoRef.current.src = "";
-    }
 
     const wrapperAttributes = {
         id: "header-logo",
 
-        children: <>
-            <img src={groupHeaderUrl(authContext.organization)}
-                className="organization-logo" onError={imgLogoLoadError}
-                ref={imageLogoRef} title={authContext.organization?.name}
-            />
-
-            { imageLogoRef.current?.src === undefined && authContext.organization.name }
-        </>
+        children: <img
+            src={groupHeaderUrl(authContext.organization)}
+            className="organization-logo"
+            alt={authContext.organization.name}
+            title={authContext.organization.name}
+        />
     };
 
     return authContext.profile
         ? <Link {...wrapperAttributes} to={`/group${authContext.organization.path}`} />
-        : <p {...wrapperAttributes}/>
+        : <div {...wrapperAttributes}/>
 }
