36c5d36b0862b30a8d5eb2bc112409e8b02e1a73
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2024-08-25 18:23:36 -0300

    refactor: providers on App instead of main
    move AuthProvider and YouTubePlayerProvider from /src/App.tsx to /src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import {RouterProvider} from 'react-router-dom'
 
 import { router } from "@/services/routes";
-import { AuthProvider } from "@/contexts/Auth";
-import { YouTubePlayerProvider } from "@/contexts/YouTube";
 
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import 'sweetalert2/dist/sweetalert2.min.css'
@@ -13,11 +11,7 @@ import '@/layouts/global.less'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AuthProvider>
-        <YouTubePlayerProvider>
-            <RouterProvider router={router}/>
-        </YouTubePlayerProvider>
-    </AuthProvider>
+    <RouterProvider router={router}/>
 
     <div id="modal-container" />
   </React.StrictMode>
