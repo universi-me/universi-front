6b325385607a34fc22076d0870316d8e6c37dce7
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2025-02-04 18:18:18 -0300

    refactor: update UniversimeApi.Job

--- src/services/UniversimeApi/index.ts
+++ src/services/UniversimeApi/index.ts
@@ -18,5 +18,6 @@ export * as GroupTheme from "./GroupTheme";
 export * as Health from "./Health";
 export * as Image from "./Image";
 export * as Institution from "./Institution";
+export * as Job from "./Job";
 export * as Profile from "./Profile";
 export * as User from "./User";
