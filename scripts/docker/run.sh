3a5d4ceacf78d7e84b1d1be1b45b473ea75f1315
Julio Verne <55258516+julio-ufpb@users.noreply.github.com>
2025-02-25 20:51:43 -0300

    change: add use default value in environment run script

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -2,23 +2,23 @@
 
 replace_var() {
     local var_name=$1
-    local default_value=$2
-    local var_value=${!var_name:-$default_value}
+    local value_var=$2
+    local default_value_var=$3
 
-    if [[ -z "$var_name" ]]; then
-        echo "Erro: A variável '$var_name' não foi encontrada ou está vazia."
-        exit 1
-    else
-        echo "Substituindo a variável de ambiente '$var_name' pelo valor '$var_value'."
-        grep -sRl "@${var_name}@" /usr/share/nginx/html/universime | xargs sed -i -e "s|@${var_name}@|${var_value}|g"
+    local var_value=${!var_name:-$value_var}
+    if [ -z "$var_value" ]; then
+        var_value=$default_value_var
     fi
+
+    echo "Substituindo a variável de ambiente '$var_name' pelo valor '$var_value'."
+    grep -sRl "@${var_name}@" /usr/share/nginx/html/universime | xargs sed -i -e "s|@${var_name}@|${var_value}|g"
 }
 
 # Substitui todas as ocorrências de variáveis de ambiente
-replace_var 'UMAMI_URL' ''
-replace_var 'UMAMI_ID' ''
-replace_var 'UNIVERSIME_API' $VITE_UNIVERSIME_API
-replace_var 'BUILD_HASH' $VITE_BUILD_HASH
+replace_var 'UMAMI_URL' '' ''
+replace_var 'UMAMI_ID' '' ''
+replace_var 'UNIVERSIME_API' $VITE_UNIVERSIME_API 'http://localhost:8080/api'
+replace_var 'BUILD_HASH' $VITE_BUILD_HASH 'development'
 
 # Inicia o servidor nginx
 echo "Iniciando o servidor nginx."
