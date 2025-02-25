d276dc5b465c65e25ef58466987c376b1b97fd35
Julio Verne <55258516+julio-ufpb@users.noreply.github.com>
2025-02-25 19:59:50 -0300

    change: correct set environment variables

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 replace_var() {
-    if [[ -z "$1" ]]; then
-        echo "A variável $1 não foi encontrada."
+    local var_name=$1
+    local default_value=$2
+    local var_value=${!var_name:-$default_value}
+
+    if [[ -z "$var_name" ]]; then
+        echo "Erro: A variável '$var_name' não foi encontrada ou está vazia."
         exit 1
     else
-        echo "Substituindo a variável de ambiente $1 pelo valor ${!1:-$2}."
-        sed -i 's|@'$1'@|'${!1}'|g' $(grep -sRl "@$1@" /usr/share/nginx/html/universime)
+        echo "Substituindo a variável de ambiente '$var_name' pelo valor '$var_value'."
+        grep -sRl "@${var_name}@" /usr/share/nginx/html/universime | xargs sed -i -e "s|@${var_name}@|${var_value}|g"
     fi
 }
 
 # Substitui todas as ocorrências de variáveis de ambiente
 replace_var 'UMAMI_URL' ''
 replace_var 'UMAMI_ID' ''
+replace_var 'UNIVERSIME_API' $VITE_UNIVERSIME_API
+replace_var 'BUILD_HASH' $VITE_BUILD_HASH
 
 # Inicia o servidor nginx
 echo "Iniciando o servidor nginx."
