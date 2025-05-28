9652ec2a93cef7340680783163c62e2ac7b5782f
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2025-05-28 19:07:31 -0300

    feat: DepartmentPage use UniversiForm2

--- src/pages/Settings/EntitiesPage/DepartmentPage/DepartmentPage.tsx
+++ src/pages/Settings/EntitiesPage/DepartmentPage/DepartmentPage.tsx
@@ -8,7 +8,7 @@ import { useEffect, useMemo, useState } from "react";
 import { DepartmentPageContext } from "./DepartmentPageContext";
 import { UniversimeApi } from "@/services";
 import ActionButton from "@/components/ActionButton";
-import UniversiForm, { FormInputs } from "@/components/UniversiForm";
+import UniversiForm from "@/components/UniversiForm2";
 import stringUtils from "@/utils/stringUtils";
 
 import "./DepartmentPage.less";
@@ -50,18 +50,20 @@ export function DepartmentPage() {
             </section>
             { departments !== null && !creatingDepartment && <ActionButton name="Criar órgão/área" buttonProps={{ onClick: () => setCreatingDepartment( true ) }} /> }
 
-            { creatingDepartment && <UniversiForm
-                formTitle="Criar órgão/área" saveButtonText="Criar"
-                objects={[
-                    { DTOName: "acronym", label: "Sigla", type: FormInputs.TEXT, required: true },
-                    { DTOName: "name", label: "Nome", type: FormInputs.TEXT, required: true },
-                ]}
-                requisition={ ( form: CreateDepartmentForm ) => UniversimeApi.Department.create( form ) }
-                callback={ async () => {
-                    await refreshDepartments();
-                    setCreatingDepartment( false );
-                } }
-            /> }
+            { creatingDepartment && <UniversiForm.Root title="Criar órgão/área" confirmButtonText="Criar" callback={ createDepartment }>
+                <UniversiForm.Input.Text
+                    param="acronym"
+                    label="Sigla"
+                    required
+                />
+
+                <UniversiForm.Input.Text
+                    param="name"
+                    label="Nome"
+                    required
+                />
+            </UniversiForm.Root>
+            }
         </div>
     </DepartmentPageContext.Provider>
 
@@ -69,9 +71,20 @@ export function DepartmentPage() {
         const res = await UniversimeApi.Department.list();
         setDepartments( res.body ?? null );
     }
+
+    async function createDepartment( form: CreateDepartmentForm ) {
+        if ( !form.confirmed ) {
+            setCreatingDepartment( false );
+            return;
+        }
+
+        const res = await UniversimeApi.Department.create( form.body );
+        await refreshDepartments();
+        setCreatingDepartment( false );
+    }
 }
 
-type CreateDepartmentForm = {
+type CreateDepartmentForm = UniversiForm.Data<{
     acronym: string;
     name: string;
-};
+}>;
