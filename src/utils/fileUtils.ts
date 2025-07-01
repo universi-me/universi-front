a058627a466218899364e3c9fe66b8428328c177
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2025-07-01 10:54:08 -0300

    feat: add resources usage table to HealthCheckPage

--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -11,3 +11,19 @@ export function arrayBufferToBase64(buf: ArrayBuffer): string {
         ? Buffer.from(buf).toString('base64')    // Server-side
         : window.btoa(arrayBufferToBinary(buf)); // Client-side
 }
+
+const MEMORY_UNITS = [ "KB", "MB", "GB" ];
+export function convertBytes( size: number, precision: number = 2 ): string {
+    if ( size < 0 )
+        return "-" + convertBytes( -size, precision );
+
+    let unit = "B";
+    for ( const memUnit of MEMORY_UNITS ) {
+        if ( size < 1024 ) break;
+
+        size /=1024;
+        unit = memUnit;
+    }
+
+    return `${ size.toFixed( precision ) } ${ unit }`;
+}
